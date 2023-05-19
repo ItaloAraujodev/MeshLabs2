11b6e6812d76e1d4d1d27e8292ebd1448bbbd7bd
ItaloAraujoDev <italoarauju2019@gmail.com>
2023-05-19 15:16:00 -0300

    Teste 2

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    reactStrictMode: true,
+    swcMinify: true,
+    trailingSlash: true,
+    unoptimized: true,
+    domains: ['meshlabs.site'],
+  },
+  experimental: {
+    appDir: true,
+  },
+}
 
 module.exports = nextConfig
